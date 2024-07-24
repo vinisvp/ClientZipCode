8ce45da9473131234aa379555b2f0f96ba21aebe
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-07-24 19:12:32 -0300

    closes #4 - The form show the zip code info

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-$("#inputZipCode").mask("00000-000");
+$(document).ready(function(){
+    $("#inputZipCode").mask("00000-000");
+})
+
+function fillTheForm(address){
+    document.getElementById("inputAddress").value =  address.logradouro;
+    document.getElementById("inputDistrict").value = address.bairro;
+    document.getElementById("inputCity").value =     address.localidade;
+    document.getElementById("inputState").value =    address.uf;
+}
+
+function disableNumber(option){
+    $("#inputNum").prop("disabled", option);
+}
+
+function showZipCode(){
+    var zip = document.getElementById("inputZipCode").value;
+    var url = `https://viacep.com.br/ws/${zip}/json/`;
+    
+    $.getJSON(url, (address) => {
+        fillTheForm(address);
+        disableNumber(false);
+    });
+}
