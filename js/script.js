824e5b646dc1adbf6974f4be99ad965729ffaf91
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-07-24 20:51:11 -0300

    closes #6 - Fixed errors, and made the page responsive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 var clients = [];
+var valid = false
 
 $(document).ready(function(){
     $("#inputZipCode").mask("00000-000");
 });
 
 function fillTheForm(address){
-    document.getElementById("inputAddress").value =  address.logradouro;
-    document.getElementById("inputDistrict").value = address.bairro;
-    document.getElementById("inputCity").value =     address.localidade;
-    document.getElementById("inputState").value =    address.uf;
+    document.getElementById("inputAddress").value =  address.logradouro || "";
+    document.getElementById("inputDistrict").value = address.bairro     || "";
+    document.getElementById("inputCity").value =     address.localidade || "";
+    document.getElementById("inputState").value =    address.uf         || "";
 }
 
 function disableNumber(option){
     $("#inputNum").prop("disabled", option);
+}
+
+function invalidZipCode(msg){
+    fillTheForm({});
+    disableNumber(true);
+    valid = false;
+    document.getElementById("error").innerHTML = msg;
 }
 
 function showZipCode(){
@@ -20,8 +28,19 @@ function showZipCode(){
     var url = `https://viacep.com.br/ws/${zip}/json/`;
     
     $.getJSON(url, (address) => {
-        fillTheForm(address);
-        disableNumber(false);
+        if (!("erro" in address))
+        {
+            fillTheForm(address);
+            disableNumber(false);
+            valid = true;
+            document.getElementById("error").innerHTML = "";
+        }
+        else
+        {
+            invalidZipCode("*CEP não encontrado");
+        }
+    }).fail(() => {
+        invalidZipCode("*CEP Inválido");
     });
 }
 
@@ -37,7 +56,7 @@ function addClientTable(client){
 
     node = document.createTextNode(client.address);
     var cell = row.insertCell();
-    cell.className = "d-none d-lg-table-cell";
+    cell.className = "d-none d-md-table-cell";
     cell.appendChild(node);
 
     node = document.createTextNode(client.ZipCode);
@@ -60,21 +79,25 @@ function addClientTable(client){
 }
 
 function addClient(){
-    var fullName = document.getElementById("inputName").value
-    + " " +        document.getElementById("inputSurName").value;
-    
-    var client = {
-        id :       clients.length + 1,
-        name :     fullName,
-        address :  `${document.getElementById("inputAddress").value}, ${document.getElementById("inputNum").value}`,
-        ZipCode :  document.getElementById("inputZipCode").value,
-        district : document.getElementById("inputDistrict").value,
-        city :     document.getElementById("inputCity").value,
-        state :    document.getElementById("inputState").value
-    }
+    if (valid)
+    {
+        var fullName = document.getElementById("inputName").value
+        + " " +        document.getElementById("inputSurName").value;
+        
+        var client = {
+            id :       clients.length + 1,
+            name :     fullName,
+            address :  `${document.getElementById("inputAddress").value}, ${document.getElementById("inputNum").value}`,
+            ZipCode :  document.getElementById("inputZipCode").value,
+            district : document.getElementById("inputDistrict").value,
+            city :     document.getElementById("inputCity").value,
+            state :    document.getElementById("inputState").value
+        }
 
-    clients.push(client);
-    addClientTable(client);
-    document.getElementById("formClient").reset();
-    disableNumber(true);
+        clients.push(client);
+        addClientTable(client);
+        document.getElementById("formClient").reset();
+        disableNumber(true);
+        valid = false;
+    }  
 }
