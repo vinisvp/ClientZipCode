b307c4c94a75d5a785b119d63590c8b4efebed6f
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-07-24 19:58:32 -0300

    closes #5 - Now you can add a client in the table

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+var clients = [];
+
 $(document).ready(function(){
     $("#inputZipCode").mask("00000-000");
-})
+});
 
 function fillTheForm(address){
     document.getElementById("inputAddress").value =  address.logradouro;
@@ -21,4 +23,58 @@ function showZipCode(){
         fillTheForm(address);
         disableNumber(false);
     });
+}
+
+function addClientTable(client){
+    var table = document.getElementById("clientsTable");
+    var row = table.insertRow();
+
+    var node = document.createTextNode(client.id);
+    row.insertCell().appendChild(node);
+
+    node = document.createTextNode(client.name);
+    row.insertCell().appendChild(node);
+
+    node = document.createTextNode(client.address);
+    var cell = row.insertCell();
+    cell.className = "d-none d-lg-table-cell";
+    cell.appendChild(node);
+
+    node = document.createTextNode(client.ZipCode);
+    row.insertCell().appendChild(node);
+
+    node = document.createTextNode(client.district);
+    cell = row.insertCell();
+    cell.className = "d-none d-lg-table-cell";
+    cell.appendChild(node);
+
+    node = document.createTextNode(client.city);
+    cell = row.insertCell();
+    cell.className = "d-none d-lg-table-cell";
+    cell.appendChild(node);
+
+    node = document.createTextNode(client.state);
+    cell = row.insertCell();
+    cell.className = "d-none d-lg-table-cell";
+    cell.appendChild(node);
+}
+
+function addClient(){
+    var fullName = document.getElementById("inputName").value
+    + " " +        document.getElementById("inputSurName").value;
+    
+    var client = {
+        id :       clients.length + 1,
+        name :     fullName,
+        address :  `${document.getElementById("inputAddress").value}, ${document.getElementById("inputNum").value}`,
+        ZipCode :  document.getElementById("inputZipCode").value,
+        district : document.getElementById("inputDistrict").value,
+        city :     document.getElementById("inputCity").value,
+        state :    document.getElementById("inputState").value
+    }
+
+    clients.push(client);
+    addClientTable(client);
+    document.getElementById("formClient").reset();
+    disableNumber(true);
 }
